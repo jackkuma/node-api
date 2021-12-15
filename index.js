e62b38499aff9d0de6432f54deca2675c81571b7
jackkuma <jackal916@gmail.com>
2021-12-15 09:05:33 +0800

    add CORS

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var bodyparser = require('body-parser'); //解析HTTP請求的中介軟體
 var express = require('express');
+var cors = require('cors');
 
 var conf = require('./conf');
 var functions = require('./functions');
 var lots = require('./routes/parkingLots');
 
 var app = express();
+
+const corsOptions = {
+  origin: [
+    'http://localhost',
+    'http://localhost:3000',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 // 使用bodyparser.json()將HTTP請求方法 POST、DELETE、PUT和PATCH，放在HTTP主體(body); 發送的參數存放在req.body
 app.use(bodyparser.urlencoded({extended: false}));
